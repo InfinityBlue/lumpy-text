d5135692ab497594c5ad64bd2dc93e47eeff82a0
何洋 <abruzzi.hraig@gmail.com>
2014-08-24 20:32:42 +0800

    update: add introductions of 'direction' and 'more color'

--- javascripts/main.js
+++ javascripts/main.js
@@ -1,7 +1,18 @@
 $(function() {
-    $('.lumpy').lumpy({
-        begin: '#eee',
-        end: '#333',
-        steps: 200
+    $('.lumpy-color').lumpy({
+        begin: '#fff',
+        end: '#444',
+        steps: 60
+    });
+    $('.lumpy-direction').lumpy({
+        begin: '#444',
+        end: '#fff',
+        steps: 60,
+        direction: 'begin'
+    });
+    $('.lumpy-more-color').lumpy({
+        begin: '#ff87a3',
+        end: '#00fa64',
+        steps: 100,
     });
 });
